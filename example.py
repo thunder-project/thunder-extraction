47fcf57928939aae783c827e60ddefffc85d1257
jasminetstone <jasminetstone@gmail.com>
2016-07-13 10:45:40 -0400

    add cnmf file

--- example.py
+++ example.py
@@ -3,15 +3,24 @@
 from extraction.utils import make_gaussian
 data = make_gaussian(noise=0.5)
 
-# fit a model
+# fit an nmf model
 
 from extraction import NMF
-model = NMF().fit(data, chunk_size=(100,200))
+nmfmodel = NMF().fit(data, chunk_size=(100,200))
+
+# OR
+
+#fit a cnmf model
+from extraction import cnmf
+cnmfmodel = CNMF().fit(data, chunk_size=(100,200))
 
 # show estimated sources
 
 import matplotlib.pyplot as plt
 from showit import image
 
-image(model.regions.mask((100, 200), fill=None, stroke='deeppink', base=data.mean().toarray() / 2))
+image(nmfmodel.regions.mask((100, 200), fill=None, stroke='deeppink', base=data.mean().toarray() / 2))
+plt.show()
+
+image(cnmfmodel.regions.mask((100, 200), fill=None, stroke='deeppink', base=data.mean().toarray() / 2))
 plt.show()
