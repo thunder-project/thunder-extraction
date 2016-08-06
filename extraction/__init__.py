5cd2334737b0ba75909721ab44c32d52f3eed875
freeman <the.freeman.lab@gmail.com>
2016-08-06 12:13:40 -0400

    1.2.0

--- extraction/__init__.py
+++ extraction/__init__.py
@@ -1,4 +1,4 @@
-__version__='1.1.0'
+__version__='1.2.0'
 
 from .algorithms.nmf import NMF
 from .utils import load
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-version = '1.1.0'
+version = '1.2.0'
 
 required = open('requirements.txt').read().split('\n')
 
